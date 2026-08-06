c62ca0385dc102ca2a8e5e685563cb4ec0e8f4f9
Hamza ElNahtawy <hamza.nahtawy@gmail.com>
2026-08-07 00:33:03 +0300

    got rid of haar cascades and began using yunet dnn

---
+++ README.md
@@ -0,0 +1,22 @@
+# Face Recognition System
+
+A real-time face recognition system built in Python using OpenCV and modern computer vision techniques.
+
+## Features
+
+- Live webcam feed
+- Face detection
+- Face recognition
+- Real-time bounding boxes
+- Future AI security camera features
+
+## Technologies
+
+- Python
+- OpenCV
+- NumPy
+- face_recognition / dlib (planned)
+
+## Status
+
+🚧 Work in progress - Contributions Welcome
---
+++ detect_face.py
@@ -0,0 +1,34 @@
+import cv2
+
+model_path = "weights/face_detection_yunet_2026may.onnx"
+detector = cv2.FaceDetectorYN.create(model_path, "", (640,480))
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Camera cannot be opened")
+    exit()
+
+print("Webcam opened succesfully")
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
+
+    if not ret:
+        print("cannot receive frame")
+        break
+
+
+    retval, facesDetected = detector.detect(frame)
+
+    if facesDetected is not None:
+        for face in facesDetected:
+            x, y, w, h = map(int, face[0:4])
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
+
+    cv2.imshow('yalahwiiiii', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
