ee3c3836c47daad9ce2ba3f08534135399c77dcf
Hamza ElNahtawy <hamza.nahtawy@gmail.com>
2026-08-17 16:01:15 +0300

    Added support for multiple people, labeling for recognized faces, some code formatting, getting very close to final goal :)

--- create_encodings.py
+++ create_encodings.py
@@ -1,38 +1,54 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
 
 detector_path = "weights/face_detection_yunet_2026may.onnx"
 recognizer_path = "weights/face_recognition_sface_2021dec.onnx"
-folder_path = "knownFaces"
+dataset_path = "test_images"             #TODO: Change to known_faces
+encoding_path = "test_encodings"         #TODO: Change to encodings
 
-detector = cv2.FaceDetectorYN.create(
+
+detector = cv.FaceDetectorYN.create(
     detector_path,
     "",
     (320, 320),
     0.8,
-    0.3,
-    5000
 )
-recognizer = cv2.FaceRecognizerSF.create(recognizer_path, "")
 
-count = 1 
+recognizer = cv.FaceRecognizerSF.create(
+    recognizer_path,
+    ""
+)
 
-if os.path.exists(folder_path):
-    for file in os.listdir(folder_path):
-        if file.lower().endswith((".jpg", ".png")):
-            full_path = f"{folder_path}/{file}"
-            npImage = cv2.imread(full_path)
-            h, w, _ = npImage.shape
-            detector.setInputSize((w,h))
-            retval, detectedFace = detector.detect(npImage)
-            if detectedFace is None:
-                print(f"{file}: No face detected")
-            elif len(detectedFace) == 1:
-                print(f"{file}: 1 face detected")
-            else:
-                print(f"{file}: {len(detectedFace)} faces detected")
-            if detectedFace is not None and len(detectedFace) == 1:
-                feature = recognizer.feature(recognizer.alignCrop(npImage, detectedFace))
-                np.save(f"encodings/{count}.npy", feature)
-                count += 1
+count = 0 
+
+if os.path.exists(dataset_path):
+    for folder in os.listdir(dataset_path):
+        full_folder_path = os.path.join(dataset_path, folder)
+
+        if os.path.isdir(full_folder_path):
+            os.mkdir(os.path.join(encoding_path, folder))
+
+            for image in os.listdir(full_folder_path):
+
+                if image.lower().endswith((".jpg", ".jpeg", ".png")):
+                    full_image_path = os.path.join(full_folder_path, image)
+                    npImage = cv.imread(full_image_path)
+                    h, w, _ = npImage.shape
+                    detector.setInputSize((w, h))
+                    retval, detectedFace = detector.detect(npImage)
+
+                    if detectedFace is None:
+                        print(f"{full_image_path}: No face detected")
+
+                    elif len(detectedFace) == 1:
+                        print(f"{full_image_path}: 1 face detected")
+                        feature = recognizer.feature(recognizer.alignCrop(npImage, detectedFace))
+                        np.save(f"{encoding_path}/{folder}/{count}.npy", feature)
+                        count += 1
+                        
+                    else:
+                        print(f"{full_image_path}: {len(detectedFace)} faces detected")
+
+else:
+    print("Dataset path not found")
--- face_recognition.py
+++ face_recognition.py
@@ -1,45 +1,78 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import os
 
-detector_path = "weights/face_detection_yunet_2026may.onnx"
-recognizer_path = "weights/face_recognition_sface_2021dec.onnx"
+DETECTOR_PATH = "weights/face_detection_yunet_2026may.onnx"
+RECOGNIZER_PATH = "weights/face_recognition_sface_2021dec.onnx"
+ENCODING_FOLDER_PATH = "test_encodings"
 
-detector = cv2.FaceDetectorYN.create(detector_path, "", (640,480))
-recognizer = cv2.FaceRecognizerSF.create(recognizer_path, "")
-cap = cv2.VideoCapture(0)
 
-if not cap.isOpened():
+detector = cv.FaceDetectorYN.create(
+    DETECTOR_PATH,
+    "",
+    (640,480)
+)
+
+recognizer = cv.FaceRecognizerSF.create(
+    RECOGNIZER_PATH,
+    ""
+)
+
+camera = cv.VideoCapture(0)
+
+if not camera.isOpened():
     print("Camera cannot be opened")
     exit()
 
-print("Webcam opened succesfully")
+print("Camera opened succesfully")
 
-while True:
-    ret, frame = cap.read()
-    frame = cv2.flip(frame, 1)
+try:
+    while True:
+        ret, frame = camera.read()
+        frame = cv.flip(frame, 1)
 
-    if not ret:
-        print("Cannot receive frame")
-        break
+        if not ret:
+            print("Cannot receive frame")
+            break
 
-    retval, facesDetected = detector.detect(frame)
+        _, faces_detected = detector.detect(frame)
 
-    if facesDetected is not None:
-        for face in facesDetected:
-            alignedFace = recognizer.alignCrop(frame, face)
-            feature = recognizer.feature(alignedFace)
-            for encoding in os.listdir("encodings"):
-                score = recognizer.match(feature, np.load(f"encodings/{encoding}"), cv2.FaceRecognizerSF_FR_COSINE)
-                if score > 0.7:
-                    print("Recognized face")
-            x, y, w, h = map(int, face[0:4])
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
+        if faces_detected is not None:
+            for face in faces_detected:
+                aligned_face = recognizer.alignCrop(frame, face)
+                feature = recognizer.feature(aligned_face)
+
+                for person in os.listdir(ENCODING_FOLDER_PATH):
+                    full_folder_path = os.path.join(ENCODING_FOLDER_PATH, person)
+                    if os.path.isdir(full_folder_path):
+                        for file in os.listdir(full_folder_path):
+
+                            score = recognizer.match(
+                                feature, 
+                                np.load(
+                                    os.path.join(
+                                        ENCODING_FOLDER_PATH, 
+                                        person, 
+                                        file
+                                    )
+                                ), 
+                                cv.FaceRecognizerSF_FR_COSINE
+                            )
+
+                            x, y, w, h = map(int, face[0:4])
+                            cv.rectangle(frame, (x, y), (x + w, y + h), (0,255,0), 2)
+                            
+                            if score > 0.7:
+                                print(f"Recognized face: {person}")
 
 
-    cv2.imshow('Face detection', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+                                cv.putText(frame, person, (x, y - 15), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,0),  2, cv.LINE_AA)
 
-cap.release()
-cv2.destroyAllWindows()
+
+        cv.imshow('Face detection & recognition - Hamza ElNahtawy', frame)
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+finally:
+    camera.release()
+    cv.destroyAllWindows()
